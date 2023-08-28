0555595ac19c968847085b67d7f832c687e8d22d
Yomi Ikuru <captyomjnr@gmail.com>
2023-08-28 21:33:23 +0100

    0.2.0 release with improvements. See README

--- src/extension.ts
+++ src/extension.ts
@@ -29,7 +29,6 @@ function fetchItems(
         }
         const lines = stdout.split(/\n/).filter((l) => l !== "");
         if (!lines.length) {
-          vscode.window.showInformationMessage("There are no items.");
           return resolve([]);
         }
         return resolve(
@@ -64,52 +63,95 @@ function fetchItems(
 }
 
 export function activate(context: vscode.ExtensionContext) {
+  let query: string[];
+  const scrollBack: QuickPickItemWithLine[] = [];
+
   (async () => {
     const disposable = vscode.commands.registerCommand(
       "livegrep.search",
       async () => {
-        const query = await vscode.window.showInputBox({
-          prompt: "Please input search word.",
-        });
+        const quickPick = vscode.window.createQuickPick();
+        quickPick.placeholder = "Please enter a search term";
+        quickPick.matchOnDescription = true;
+
         const isOption = (s: string) => /^--?[a-z]+/.test(s);
-        if (!query) {
-          vscode.window.showErrorMessage("No search term has been entered.");
-          return;
-        }
-        const q = query.split(/\s/).reduce((acc, c, i) => {
-          if (i === 0 || isOption(c) || isOption(acc[acc.length - 1])) {
-            acc.push(c);
-            return acc;
+        const isWordQuoted = (s: string) => /^".*"/.test(s);
+
+        quickPick.items = scrollBack;
+
+        quickPick.onDidChangeValue(async (value) => {
+          if (!value || value === "") {
+            return;
           }
-          acc[acc.length - 1] = acc[acc.length - 1] + ` ${c}`;
-          return acc;
-        }, [] as string[]);
-        const command = quote([rgPath, "-n", ...q, "."]);
-        const options: vscode.QuickPickOptions = { matchOnDescription: true };
-        const item = await vscode.window.showQuickPick(
-          fetchItems(command, projectRoot),
-          options
-        );
-        if (!item) {
-          return;
-        }
-        const { detail, num } = item;
-        const doc = await vscode.workspace.openTextDocument(
-          projectRoot + "/" + detail
-        );
-        await vscode.window.showTextDocument(doc);
-        if (!vscode.window.activeTextEditor) {
-          vscode.window.showErrorMessage("No active editor.");
-          return;
-        }
-        vscode.window.activeTextEditor.selection = new vscode.Selection(
-          ~~num,
-          0,
-          ~~num,
-          0
-        );
-        vscode.commands.executeCommand("cursorUp");
-        context.subscriptions.push(disposable);
+          query = value.split(/\s/).reduce((acc, curr, index) => {
+            if (
+              index === 0 ||
+              isOption(curr) ||
+              isOption(acc[acc.length - 1])
+            ) {
+              if (!isWordQuoted(curr) && !isOption(curr)) {
+                acc.push("-i", curr); // add case insensitive flag
+                return acc;
+              }
+              acc.push(curr.replace(/"/g, "")); // remove quotes
+              return acc;
+            }
+            acc[acc.length - 1] = acc[acc.length - 1] + ` ${curr}`;
+            return acc;
+          }, [] as string[]);
+          quickPick.items = await fetchItems(
+            quote([rgPath, "-n", ...query, "."]),
+            projectRoot
+          );
+        });
+
+        quickPick.onDidAccept(async () => {
+          // Create scrollback item when user makes selection
+          const scrollBackItem = {
+            label: query.join(" "),
+            description: "History",
+            num: scrollBack.length + 1,
+          };
+
+          // Scrollback history is limited to 10 items
+          if (scrollBack.length > 10) {
+            // remove oldest item
+            scrollBack.shift();
+          }
+          scrollBack.unshift(scrollBackItem);
+
+          const item = quickPick.selectedItems[0] as QuickPickItemWithLine;
+          if (!item) {
+            return;
+          }
+          if (item.description === "History") {
+            // Add ability to select history item to replace current search
+            quickPick.items = await fetchItems(
+              quote([rgPath, "-n", ...item.label.split(/\s/), "."]),
+              projectRoot
+            );
+            return;
+          }
+          const { detail, num } = item;
+          const doc = await vscode.workspace.openTextDocument(
+            projectRoot + "/" + detail
+          );
+          await vscode.window.showTextDocument(doc);
+          if (!vscode.window.activeTextEditor) {
+            vscode.window.showErrorMessage("No active editor.");
+            return;
+          }
+          vscode.window.activeTextEditor.selection = new vscode.Selection(
+            ~~num,
+            0,
+            ~~num,
+            0
+          );
+          vscode.commands.executeCommand("cursorUp");
+          context.subscriptions.push(disposable);
+        });
+
+        quickPick.show();
       }
     );
   })().catch((error) => {
