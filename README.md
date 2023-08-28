75bd1c288dcf468623fc79d631cc7b458fb20a0e
Yomi Ikuru <captyomjnr@gmail.com>
2023-08-28 13:56:25 +0100

    Update ripgrep command and fix typescript issues

--- README.md
+++ README.md
@@ -1,30 +1,15 @@
-# vscode-ripgrep
+# vscode-livegrep
 
 ## Features
 
-A ripgrep extension for Visual Studio Code.
+A ripgrep extension for Visual Studio Code to mimic Telescope in Neovim
 
-![screenshot](https://github.com/bokuweb/vscode-ripgrep/blob/master/docs/screenshot.gif?raw=true)
+![screenshot](https://github.com/abayomi185/vscode-livegrep/blob/master/docs/screenshot.gif?raw=true)
 
 ## Extension Settings
 
-## Known Issues
-
 ## Release Notes
-
-### 0.2.1
-
-- fix: rename `ripGrep` to `ripgrep`
-- fix: fixed a bug, options is not parsed properly. 
-
-### 0.1.2
-
-- fix: filter invalid data
-
-### 0.1.1
-
-- fix: ignore some options.
 
 ### 0.1.0
 
-- initial
+- Initial release
--- src/extension.ts
+++ src/extension.ts
@@ -1,89 +1,119 @@
-"use strict";
-
-import { exec } from "child_process";
+import * as vscode from "vscode";
+import * as cp from "child_process";
+import { rgPath } from "@vscode/ripgrep";
 import { quote } from "shell-quote";
-import { window, commands, ExtensionContext, workspace, Selection, QuickPickItem, QuickPickOptions } from "vscode";
-import { isNumber } from "util";
 
 const MAX_DESC_LENGTH = 1000;
 const MAX_BUF_SIZE = 200000 * 1024;
 
-const projectRoot = workspace.rootPath ? workspace.rootPath : ".";
+const workspaceFolders: string[] =
+  vscode.workspace.workspaceFolders?.map((folder) => folder.uri.fsPath) || [];
+const projectRoot = workspaceFolders[0] || ".";
 
-interface QuickPickItemWithLine extends QuickPickItem {
+interface QuickPickItemWithLine extends vscode.QuickPickItem {
   num: number;
 }
 
-function fetchItems(command: string, projectRoot: string): Promise<QuickPickItemWithLine[]> {
+function fetchItems(
+  command: string,
+  projectRoot: string
+): Promise<QuickPickItemWithLine[]> {
   return new Promise((resolve, reject) => {
-    exec(command, { cwd: projectRoot, maxBuffer: MAX_BUF_SIZE }, (err, stdout, stderr) => {
-      if (stderr) {
-        window.showErrorMessage(stderr);
-        return resolve([]);
+    cp.exec(
+      command,
+      { cwd: projectRoot, maxBuffer: MAX_BUF_SIZE },
+      (err, stdout, stderr) => {
+        if (stderr) {
+          vscode.window.showErrorMessage(stderr);
+          return resolve([]);
+        }
+        const lines = stdout.split(/\n/).filter((l) => l !== "");
+        if (!lines.length) {
+          vscode.window.showInformationMessage("There are no items.");
+          return resolve([]);
+        }
+        return resolve(
+          lines
+            .map((line) => {
+              const [fullPath, num, ...desc] = line.split(":");
+              const description = desc.join(":").trim();
+              return {
+                fullPath,
+                num: Number(num),
+                line,
+                description,
+              };
+            })
+            .filter(
+              ({ description, num }) =>
+                description.length < MAX_DESC_LENGTH && !!num
+            )
+            .map(({ fullPath, num, line, description }) => {
+              const path = fullPath.split("/");
+              return {
+                label: `${path[path.length - 1]} : ${num}`,
+                description,
+                detail: fullPath,
+                num,
+              };
+            })
+        );
       }
-      const lines = stdout.split(/\n/).filter(l => l !== "");
-      if (!lines.length) {
-        window.showInformationMessage("There are no items.");
-        return resolve([]);
-      }
-      return resolve(
-        lines
-          .map(line => {
-            const [fullPath, num, ...desc] = line.split(":");
-            const description = desc.join(":").trim();
-            return {
-              fullPath,
-              num: Number(num),
-              line,
-              description,
-            };
-          })
-          .filter(({ description, num }) => description.length < MAX_DESC_LENGTH && !!num)
-          .map(({ fullPath, num, line, description }) => {
-            const path = fullPath.split("/");
-            return {
-              label: `${path[path.length - 1]} : ${num}`,
-              description,
-              detail: fullPath,
-              num,
-            };
-          }),
-      );
-    });
+    );
   });
 }
 
-export function activate(context: ExtensionContext) {
+export function activate(context: vscode.ExtensionContext) {
   (async () => {
-    const disposable = commands.registerCommand("extension.ripgrep", async () => {
-      const query = await window.showInputBox({
-        prompt: "Please input search word.",
-      });
-      const isOption = (s: string) => /^--?[a-z]+/.test(s);
-      const q = query.split(/\s/).reduce(
-        (acc, c, i) => {
+    const disposable = vscode.commands.registerCommand(
+      "livegrep.search",
+      async () => {
+        const query = await vscode.window.showInputBox({
+          prompt: "Please input search word.",
+        });
+        const isOption = (s: string) => /^--?[a-z]+/.test(s);
+        if (!query) {
+          vscode.window.showErrorMessage("No search term has been entered.");
+          return;
+        }
+        const q = query.split(/\s/).reduce((acc, c, i) => {
           if (i === 0 || isOption(c) || isOption(acc[acc.length - 1])) {
             acc.push(c);
             return acc;
           }
           acc[acc.length - 1] = acc[acc.length - 1] + ` ${c}`;
           return acc;
-        },
-        [] as string[],
-      );
-      const command = quote([require("vscode-ripgrep").rgPath, "-n", ...q]);
-      const options: QuickPickOptions = { matchOnDescription: true };
-      const item = await window.showQuickPick(fetchItems(command, projectRoot), options);
-      if (!item) return;
-      const { detail, num } = item;
-      const doc = await workspace.openTextDocument(projectRoot + "/" + detail);
-      await window.showTextDocument(doc);
-      window.activeTextEditor.selection = new Selection(~~num, 0, ~~num, 0);
-      commands.executeCommand("cursorUp");
-      context.subscriptions.push(disposable);
-    });
-  })().catch(error => {
-    window.showErrorMessage(error);
+        }, [] as string[]);
+        const command = quote([rgPath, "-n", ...q, "."]);
+        const options: vscode.QuickPickOptions = { matchOnDescription: true };
+        const item = await vscode.window.showQuickPick(
+          fetchItems(command, projectRoot),
+          options
+        );
+        if (!item) {
+          return;
+        }
+        const { detail, num } = item;
+        const doc = await vscode.workspace.openTextDocument(
+          projectRoot + "/" + detail
+        );
+        await vscode.window.showTextDocument(doc);
+        if (!vscode.window.activeTextEditor) {
+          vscode.window.showErrorMessage("No active editor.");
+          return;
+        }
+        vscode.window.activeTextEditor.selection = new vscode.Selection(
+          ~~num,
+          0,
+          ~~num,
+          0
+        );
+        vscode.commands.executeCommand("cursorUp");
+        context.subscriptions.push(disposable);
+      }
+    );
+  })().catch((error) => {
+    vscode.window.showErrorMessage(error);
   });
 }
 
